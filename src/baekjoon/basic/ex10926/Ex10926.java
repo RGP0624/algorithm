ff16aff8ce59a1026eee94380b2e2f14d453b4dc
RGP0624 <parkcm6024@naver.com>
2024-12-14 19:03:00 +0900

    10926 풀이

---
+++ src/baekjoon/basic/ex10926/Ex10926.java
@@ -0,0 +1,16 @@
+package baekjoon.basic.ex10926;
+
+import java.util.Scanner;
+
+public class Ex10926
+{
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+
+        System.out.print(a + "??!");
+
+
+    }
+}
